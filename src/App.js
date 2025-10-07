12b2971b020a044bd3dc7c1f291ea6a8acf238fd
thxinsomnia <kompenizeko@gmail.com>
2025-10-07 13:11:40 +0700

    Patch Update 0.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import Pattern from './components/Pattern';
 import './App.css';
 import LoginPage from './Login/LoginPage';
 import RegisterPage from './Register/RegisterPage';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Loader from './components/Loader/Loader';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 4000); // 2 seconds loading
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <Pattern />
+        <Loader />
+      </>
+    );
+  }
+
   return (
+     <>
+    <Pattern />
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
@@ -13,6 +34,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
+  </>
   );
 }
 
---
+++ src/components/Pattern.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Pattern.css';
+
+const Pattern = () => {
+  return <div className="pattern-bg" />;
+};
+
+export default Pattern;
